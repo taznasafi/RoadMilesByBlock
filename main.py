58c0c866197c5f71134101965d7814dbc8d3a6ad
taznasafi <nasafi.taz@gmail.com>
2021-12-23 11:14:19 -0500

    initial commit V0.3 -- removed multiprocessing

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from bin import _01A_unzip_census_edges, \
     _02_import_roads_from_edges_calculate_length, _03_make_shared_road_mile
 
 from RoadMilesByBlock import get_path, logger
-# multiprocessor
-import multiprocessing as mp
+
 import time
 
 
@@ -22,38 +21,28 @@ def make_initial_data(unzip_edges=False, unzip_faces=False, make_faces=False):
         _01C_make_faces_table.import_table_from_faces(run=True)
 
 
-def worker(state):
-    start = time.time()
-    print("\n********************************** State {} ***************************************\n".format(state))
-    print('creating roads for state {}\n'.format(state))
-    _02_import_roads_from_edges_calculate_length.make_roads(run=True, calculate_fields=True, state_fips=state, gdb_name="tl_roads_from_edges")
-    print("calculating shared miles for state {}\n".format(state))
-    _03_make_shared_road_mile.create_shared_road_miles(run=True, state_fips=state)
-
-    end = time.time()
-
-    logger.timer(start, end, state)
 
 
 if __name__ == '__main__':
     print("hi")
     print("\n****************************** making initial Faces & Edges **************************\n")
     start = time.time()
-    make_initial_data(unzip_edges=True, unzip_faces=False)
-    make_initial_data(make_faces=True)
+    make_initial_data(unzip_edges=False, unzip_faces=False)
+    make_initial_data(make_faces=False)
     logger.timer(start, time.time())
 
     print("\n****************************** making shared road miles by state **************************\n")
 
-    mp.set_start_method("spawn")
-    num_process = 1
-    pool = mp.Pool(processes=num_process)
-    pool.map(worker, get_path.pathFinder.make_fips_list())
-    pool.close()
-    pool.join()
-    end = time.time()
+    for state in get_path.pathFinder.make_fips_list():
+        start = time.time()
+        print("\n********************************** State {} ***************************************\n".format(state))
+        print('creating roads for state {}\n'.format(state))
+        _02_import_roads_from_edges_calculate_length.make_roads(run=True, calculate_fields=True, state_fips=state,
+                                                                gdb_name="tl_roads_from_edges")
+        print("calculating shared miles for state {}\n".format(state))
+        _03_make_shared_road_mile.create_shared_road_miles(run=True, state_fips=state)
 
-    print("\n****************************** end of the tasks **************************\n")
-    print("multi-processing {} number of states".format(num_process))
-    logger.timer(start, end)
+        end = time.time()
+
+        logger.timer(start, end, state)
 
